f29eb30b5b1bc5c697b53d9fa1bc31fac5abd464
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-06-19 17:41:42 +0530

    feat: build out the subgraph to implement proof-of-liabilities

---
+++ index.ts
@@ -0,0 +1,282 @@
+import { ApolloServer, gql } from "apollo-server"
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
+import { buildSubgraphSchema } from "@apollo/subgraph"
+import { createProof } from "proof-of-liabilities"
+// import { readFileSync } from "fs"
+// const tree = JSON.parse(readFileSync("/home/vaibhav/Desktop/tree.json", "utf8"))
+
+// Also tested it using a tree.json file. Works the same way as expected.
+const tree = [
+  [
+    {
+      hash: "a8e4c27eae9d083120c16bf000e5c7713dc899c96464304d8a40f2bd04dffd50",
+      sum: 807423989,
+    },
+  ],
+  [
+    {
+      hash: "58313770f3391e12801ba635086663f46616886af4177a2a93b5839731901962",
+      sum: 524259875,
+    },
+    {
+      hash: "35767ff8ea4ff88e6aefc99bed4a5493fdb9b71f49404e2d364df59d6f3c2575",
+      sum: 283164114,
+    },
+  ],
+  [
+    {
+      hash: "a7091edebb4f6cba0c5289a40b40b8d3e234a91e1b48a1c8fc6cb357d0f11132",
+      sum: 522571269,
+    },
+    {
+      hash: "a45f0eeafb7ac175d7863256e6e7acaf6454a2e1b43dbe5610d9efd1f35c84f5",
+      sum: 1688606,
+    },
+    {
+      hash: "0159d663e729fbcdab27b7cc5429e7cdf3ad09e249d04bcf213c0fd0f4efdca2",
+      sum: 94410337,
+    },
+    {
+      hash: "731a9302d4c133b8b62530106c98813747dc73679dc1844f0e7df1a112994a0c",
+      sum: 188753777,
+    },
+  ],
+  [
+    {
+      hash: "f0e77599f1137691711852b61a1771f8398e4847e3ac311f5cd7c57700800ab8",
+      sum: 131745429,
+    },
+    {
+      hash: "c9a8b98156e324b558dfbbb501a1fbff977c3120f4dcfd6a39c48e39e07b900c",
+      sum: 390825840,
+    },
+    {
+      hash: "94e04f462ac2d91b2fd29ba085559941e7920ff9cfaa2aac08eaba18751099c0",
+      sum: 1651705,
+    },
+    {
+      hash: "a6958fcab47a5d479f27e9d04977d5f0cf0e781d7757c5056cdd2616fefd60e7",
+      sum: 36901,
+    },
+    {
+      hash: "2a06b61528c1996d11ce8fab5dd83e358820e77748e48e769c4c1854e2c54e8c",
+      sum: 94395527,
+    },
+    {
+      hash: "ebd91d2decdb09fd94aac1de7cf0d362d50a42227c55f72387b222821d4c47a4",
+      sum: 14810,
+    },
+    {
+      hash: "8d2ca5a1036186506d1797b6cdb42fd1ca7aab9356d369030cd05be181785cda",
+      sum: 18126,
+    },
+    {
+      hash: "324440b9d7e39e42c1f3f792a8e13f41f1c7f700672d12de113208413bd3c277",
+      sum: 188735651,
+    },
+  ],
+  [
+    {
+      hash: "ec13b19ec7ae0439e06df3646cce356c6570536480625e925cd5e48e7d20686a",
+      sum: 8251083,
+    },
+    {
+      hash: "0c02bda6679bb3f894fdf39088d9fe1452a3c7a96037b8ca54129248f383bc13",
+      sum: 123494346,
+    },
+    {
+      hash: "bb2f3b3484bd7aa0dfc4cbd73d268e2531d4d59cc8daf3963d3e131fc91ea27b",
+      sum: 67231,
+    },
+    {
+      hash: "c931842e447f81fbaea749f2fb0e7757c702877eb9e17109c9e2d6cbd55554c9",
+      sum: 390758609,
+    },
+    {
+      hash: "508fa63741cfdadd5106e84f8dceef3379ec3d4cb7d5ab30d9775f2b7c172239",
+      sum: 562442,
+    },
+    {
+      hash: "2688ae6dc238eb1bf3d675c097416001e1f7183fd439f4f75f6b1149c5289568",
+      sum: 1089263,
+    },
+    { hash: "65cd1d33020f3db6e71943a2b412ae29a1b83166a6ef12a396306021ee9eeff1", sum: 0 },
+    {
+      hash: "529b2bd9f7199ca6f60ee61ad26b916887919a49f2c45de86f2ef12886d739ad",
+      sum: 36901,
+    },
+    {
+      hash: "548f567d910986c552b031fed9976e5abd953241334cccecdf970f5e73192ce2",
+      sum: 93276783,
+    },
+    {
+      hash: "743b48895e98130524917dda6d6de41e565cd00be96ec29bc2ae3fbe82a1029a",
+      sum: 1118744,
+    },
+    { hash: "839f896b0c28c683ab04d3c9736e3f4ba7c0810d37b079494aa6d1d2663c8819", sum: 0 },
+    {
+      hash: "f3c4ebfa24212f0c2bca724182a4409b261571267dbffcf1b05f76bdc48c1f9a",
+      sum: 14810,
+    },
+    { hash: "d47e3aa73791694f902d8b24d9e9ba397df7e42784213125c2cb361414cf2bc1", sum: 0 },
+    {
+      hash: "93cbffc49baea54d6f8b5cb9fccb447e0c98717211e6ed88a0a022f3ccaba2dc",
+      sum: 18126,
+    },
+    {
+      hash: "8b2d1cae0c656a56a192220693dce7025bb623595ba00226577574f42fa89c9d",
+      sum: 176586385,
+    },
+    {
+      hash: "2a229eab70cbb8294294d8aa130bdf3e20b4f8bf4de1b5414c57f050773c4592",
+      sum: 12149266,
+    },
+  ],
+  [
+    {
+      hash: "48098bc72678831b97b2740e5351561137c42b771c2e4d8ffdd4c7b22a05f20f",
+      sum: 6819525,
+    },
+    {
+      hash: "0e841bf4b78318967fe0437d4f81a627b00d569fdb9c2b2acb0729dd2c58c4ee",
+      sum: 1431558,
+    },
+    {
+      hash: "5ca3f1e9ccb20e5b75522452230c368f0db83b117c550033967b25779e4b3cdc",
+      sum: 172635,
+    },
+    {
+      hash: "c65e5801c0a042fef41621aade58d7a04698ca7c67cc10077d0514c890e3d840",
+      sum: 123321711,
+    },
+    {
+      hash: "57c8efb72780813a51453af88c21db611dc369ca786a1b1eef82d2ae657a8a45",
+      sum: 67231,
+    },
+    { hash: "4f229a3d5aa5b54a016751fe1a3eb6a2940a6e45ada4533d1bfe3a223c027f71", sum: 0 },
+    {
+      hash: "e4f8d3573849fd9460c0841a98a8c6b065d2edba19975000f26dac338931fb70",
+      sum: 387096605,
+    },
+    {
+      hash: "09389860feb3f80815f6274442c3cb870a509a913fa1468a468e37c800cf8c9a",
+      sum: 3662004,
+    },
+    {
+      hash: "b243eb47cfcd08a302fa8d64813d6cc28df505549a09bb9345174f765c38cbca",
+      sum: 562442,
+    },
+    { hash: "5bf0ac54f2175e51d59eb8c713eb55affea64c50f2831b1ff5d79a0aa8363305", sum: 0 },
+    {
+      hash: "6c9d1a7079bd73818c1dceeeca7c0e1342e50aab78922eebb5d91ddb94803a05",
+      sum: 136540,
+    },
+    {
+      hash: "ade8b769133903d4ff8368901e4012e3023d85d00c9dbf2348037ebbeb395b06",
+      sum: 952723,
+    },
+    { hash: "ffd2ea7525487dad7da2bbc138b8ab17aa4c8190296d753c6e3f33a20e60d59e", sum: 0 },
+    { hash: "319e1e190f528f7e21ab9223181f9d592f415e6f57010d886401754fba9bef1f", sum: 0 },
+    { hash: "64c551f68649d0d4ded855b621cbb1af8dbd4495f649e9ac570b6c52c82cf7d2", sum: 0 },
+    {
+      hash: "218b33ebb88c177a7ea716c06c900993fbefe33b9be75b7832037c2292130f19",
+      sum: 36901,
+    },
+    {
+      hash: "ebd110b146139bbbb10de045698ed380ed5891ade9bfd9a31afa6e52601b14c8",
+      sum: 93245017,
+    },
+    {
+      hash: "a4d2123776b509a9680f612bc771e1ff4cbc1ad250dbe42d55a9988925dd8246",
+      sum: 31766,
+    },
+    { hash: "1e789005c4322fa50fb15ba53a95617cd0310d8e9bb9f0407add007b3538736d", sum: 0 },
+    {
+      hash: "0ec4865eb951af54f5f9197773d351e3eca867265bb610d582d6b483132e9d32",
+      sum: 1118744,
+    },
+    { hash: "3e8c0444030fed59e1235d85730b14c0af4c4813de8ea1b8504e15e43cc73e3f", sum: 0 },
+    { hash: "b0bed8d5051cd247bb0f0b2933c60fe3d814fbf98851777b7a27746be9d130f4", sum: 0 },
+    { hash: "51bfbbc204f4cbff3bc6ff0f915a510fb6bc4a3fca1080bc6769b3ceb8e5245a", sum: 0 },
+    {
+      hash: "420ed8890de6284736c7f0c565cb0782af55b6d480092e0e642c2dfc49c834d6",
+      sum: 14810,
+    },
+    { hash: "6b2574bffc4d81c81cead05338cc793020f50c28da597a3b9aae34024d971067", sum: 0 },
+    { hash: "0295a6452fa20ccf0fb57c5994d6634f3591ba6bf3a564211724e1e7ea271ac2", sum: 0 },
+    {
+      hash: "dda2f9e98464e2442b9900696c5a57ebc418b883ddf14a094d71d53a00ded9dd",
+      sum: 18126,
+    },
+    { hash: "c5a051d863fff5a552d482269a4b14761f777a29c9e6701ee32f4bae4c4e1b0a", sum: 0 },
+    { hash: "7f69ab146bcf9a4abca944df2cf6fc817d3a1dc072e236f1629ccdbedb80605d", sum: 0 },
+    {
+      hash: "a36b332f6168e8228a9d8d01ec2489b84ac58a4b61a13156cfcd499dab15fb24",
+      sum: 176586385,
+    },
+    {
+      hash: "2156c056b5c157239da1df8b196712b7d025bb1bce833ff0703488da5fcd4e40",
+      sum: 12149266,
+    },
+    { hash: "7f266f3a3dddc07c080061ccafe8f49a6982199b2302e47b7388e8e809220cd8", sum: 0 },
+  ],
+]
+
+const typeDefs = gql`
+  type LiabilityProof {
+    accountId: String!
+    partialLiabilityProofs: String!
+    totalBalance: Int!
+  }
+
+  type Query {
+    liabilityProof(accountId: ID!): LiabilityProof!
+  }
+`
+// For now I serialized the partialLiabilityProofs cause it looked a much better alternative.
+// We can deserialize it on the frontend and serve it to the user in the expected format.
+// Also for now the proof-of-liability library is not published on npm. So to test this out we need to use the library locally.
+
+// Some accountId's to test it. ("659b8ec0-89af-40c7-9c4f-2749ff84b6a6","dc0cd8b9-af79-421e-839f-c73c6c019f1e","b962796c-0a28-4429-8498-2cabee182130", "ac9871fa-9868-4765-be02-8a754ad5ca5a")
+// The accountId refers to the walletIds of the user's.
+//  This was done since initially I developed the library keeping in mind that the balance are linked to the accountId
+// instead of the walletIds.
+
+const resolvers = {
+  Query: {
+    liabilityProof: async (_, args: any) => {
+      const liabilityProof = await createProof(args.accountId, tree)
+      return {
+        accountId: liabilityProof.accountId,
+        partialLiabilityProofs: JSON.stringify(liabilityProof.partialLiabilityProofs),
+        totalBalance: liabilityProof.totalBalance,
+      }
+    },
+  },
+}
+const server = new ApolloServer({
+  schema: buildSubgraphSchema({ typeDefs, resolvers }),
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+})
+server
+  .listen(4004)
+  .then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+// type PartialLiabilityProof {
+//   merklePath: [MerklePath]!
+//   idx: Int!
+//   balance: Int!
+// }
+// type MerklePath {
+//   node: TreeNode!
+//   index: Int!
+// }
+// type TreeNode {
+//   hash: String!
+//   sum: Int!
+// }
