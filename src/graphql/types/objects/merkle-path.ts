7b43c4aafea2783a66506165ca295ade7b993f2d
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-06-21 01:34:51 +0530

    feat: describe the types for partialLiabilityProofs

---
+++ src/graphql/types/objects/merkle-path.ts
@@ -0,0 +1,17 @@
+import { GT } from "../../index"
+import { TreeNode } from "./tree-node"
+
+export const MerklePath = GT.Object<MerklePath>({
+  name: "MerklePath",
+  description: "A list of merkle paths for a given walletId.",
+  fields: () => ({
+    node: {
+      type: TreeNode,
+      description: "The node of the merkle path.",
+    },
+    index: {
+      type: GT.NonNull(GT.Int),
+      description: "The index of the node in the merkle path.",
+    },
+  }),
+})
---
+++ src/graphql/types/objects/tree-node.ts
@@ -0,0 +1,16 @@
+import { GT } from "../../index"
+
+export const TreeNode = GT.Object<TreeNode>({
+  name: "TreeNode",
+  description: "Represents a node of a tree.",
+  fields: () => ({
+    hash: {
+      type: GT.NonNull(GT.String),
+      description: "The hash of the node.",
+    },
+    sum: {
+      type: GT.NonNull(GT.Int),
+      description: "The sum of the node.",
+    },
+  }),
+})
