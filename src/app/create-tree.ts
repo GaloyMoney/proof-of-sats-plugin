5e3302b13bc9a482301967b2b83d3b7d743fcb49
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-06-25 01:29:18 +0530

    fix: address pr reviews

--- src/app/create-tree.ts
+++ src/app/create-tree.ts
@@ -1,14 +1,12 @@
-import { fileSystemLiabilityTreeRepository } from "../services/fileSystemLiabilityTreeRepository"
+import { fileSystemLiabilityTreeRepository } from "../services/LiabilityTreeRepository/fileSystemLiabilityTreeRepository"
 import { accounts } from "../graphql/helper"
 import { createLiabilitiesTree } from "proof-of-liabilities"
 
 export const createTree = async (): Promise<LiabilityTree | Error> => {
   const tree = await createLiabilitiesTree(accounts)
   if (tree instanceof Error) return tree
-  console.log(tree)
-  const result = await fileSystemLiabilityTreeRepository().persistNew(
+  return await fileSystemLiabilityTreeRepository().persistNew(
     tree,
     tree.merkleTree[0][0].hash,
   )
-  return result
 }
