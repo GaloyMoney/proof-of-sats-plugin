5870068cb9ac25282bc7faa4f48c6f7aa0550605
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-06-28 01:56:10 +0530

    fix(test): made changes in test after refactor

--- test/get-tree.test.ts
+++ test/get-tree.test.ts
@@ -1,8 +1,8 @@
-import { fileSystemLiabilityTreeRepository } from "../src/services/LiabilityTreeRepository/fileSystemLiabilityTreeRepository"
+import { getTree } from "../src/app/index"
 
 describe("it test if a valid tree is being returned", () => {
   it("should return a tree", async () => {
-    const tree = await fileSystemLiabilityTreeRepository().findLiabilityTree(
+    const tree = await getTree(
       "a60d7ab1ef9252cf2c44b7206b9e502777bc0ebad1a3c98941019d148b5a2f83",
     )
     console.log(tree)
