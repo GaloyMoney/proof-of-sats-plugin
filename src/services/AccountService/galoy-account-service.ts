39eac004b5083bab63a7559d497ecec50bd18d22
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-07-27 16:30:00 +0530

    fix: fetch wallet details

--- src/services/AccountService/galoy-account-service.ts
+++ src/services/AccountService/galoy-account-service.ts
@@ -1,46 +1,57 @@
 import { GALOY_GRAPHQL_ENDPOINT } from "../../config/index"
 import fetch from "node-fetch"
 
-const GALOY_GRAPHQL_ENDPOINT_QUERY = `query{
-  allWallets(walletCurrency:BTC)
-  {
+const getAccountIds = async () => {
+  const query = `query{
+    listWalletIds(walletCurrency:BTC)
+  }`
+  const response = await fetch(GALOY_GRAPHQL_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({ query }),
+  })
+  const data = await response.json()
+  return data.data.listWalletIds
+}
+
+const queryBuilderToFetchWallet = (id: string): string => {
+  return `query {
+  wallet(walletId: "${id}") {
     id
     balance
   }
 }`
+}
 
 export const GaloyAccountService = (): IAccountService => {
   const fetchAccounts = async (): Promise<Account[] | Error> => {
     try {
-      const response = await fetch(GALOY_GRAPHQL_ENDPOINT, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-        },
-        body: JSON.stringify({
-          query: GALOY_GRAPHQL_ENDPOINT_QUERY,
-        }),
-      })
-      const data = await response.json()
-      return mapToAccountLiability(data.data.allWallets)
-    } catch (error) {
-      return error
+      const accounts: Account[] = []
+      const AccountIds = await getAccountIds()
+      for (const accountId of AccountIds) {
+        const query = queryBuilderToFetchWallet(accountId)
+        const response = await fetch(GALOY_GRAPHQL_ENDPOINT, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+          body: JSON.stringify({ query }),
+        })
+        const data = await response.json()
+        const { wallet } = data.data
+        accounts.push({
+          accountId: wallet.id,
+          balance: wallet.balance,
+        })
+      }
+      return accounts
+    } catch (err) {
+      return err
     }
   }
   return { fetchAccounts }
-}
-
-const mapToAccountLiability = (
-  wallets: {
-    id: string
-    balance: number
-  }[],
-): Account[] => {
-  return wallets.map((wallet) => {
-    return {
-      accountId: wallet.id,
-      balance: wallet.balance,
-    }
-  })
 }
