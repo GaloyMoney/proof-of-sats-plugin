2b77da0929f9e2cc1e2ed24e8cdfedba0289c565
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-07-27 16:23:27 +0530

    feat: add Tree Metadata Repository

---
+++ src/services/TreeMetadataRepository/treeMetadataRepository.ts
@@ -0,0 +1,38 @@
+import pool from "../postgresql"
+
+export const TreeMetadataRepository = (): ITreeMetadataRepository => {
+  const persistNew = async (
+    treeMetadataArgs: TreeMetadataArgs,
+  ): Promise<TreeMetadata | Error> => {
+    try {
+      const { roothash, totalBalance } = treeMetadataArgs
+      const query = `INSERT INTO metadata (roothash, total_balance) VALUES ($1, $2) RETURNING *`
+      const values = [roothash, totalBalance]
+      const result = await pool.query(query, values)
+      return {
+        roothash: result.rows[0].roothash,
+        totalBalance: result.rows[0].total_balance,
+        dateCreated: result.rows[0].date_created,
+      }
+    } catch (err) {
+      return err
+    }
+  }
+  const findLatestTreeMetadata = async (): Promise<TreeMetadata | Error> => {
+    try {
+      const query = `SELECT * FROM metadata ORDER BY date_created DESC LIMIT 1`
+      const result = await pool.query(query)
+      return {
+        roothash: result.rows[0].roothash,
+        totalBalance: result.rows[0].total_balance,
+        dateCreated: result.rows[0].date_created,
+      }
+    } catch (err) {
+      return err
+    }
+  }
+  return {
+    persistNew,
+    findLatestTreeMetadata,
+  }
+}
