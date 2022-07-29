7e528078341133d6f717a4e1309b6bb32ea8fd6c
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-07-29 15:13:11 +0530

    refactor: to ensure better project structure

--- src/services/AccountService/galoy-account-service.ts
+++ src/services/AccountService/galoy-account-service.ts
@@ -1,48 +1,50 @@
 import { GALOY_GRAPHQL_ENDPOINT } from "../../config/index"
 import fetch from "node-fetch"
+import { gql } from "apollo-server"
 
-const getAccountIds = async () => {
-  const query = `query{
-    listWalletIds(walletCurrency:BTC)
-  }`
-  const response = await fetch(GALOY_GRAPHQL_ENDPOINT, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-    },
-    body: JSON.stringify({ query }),
-  })
-  const data = await response.json()
-  return data.data.listWalletIds
+const headers = {
+  "Content-Type": "application/json",
+  "Accept": "application/json",
 }
 
-const queryBuilderToFetchWallet = (id: string): string => {
-  return `query {
-  wallet(walletId: "${id}") {
-    id
-    balance
+const getAccountIds = async () => {
+  const query = gql`
+    query listWalletIds {
+      listWalletIds(walletCurrency: BTC)
+    }
+  `
+  try {
+    const response = await fetch(GALOY_GRAPHQL_ENDPOINT, {
+      method: "POST",
+      headers,
+      body: JSON.stringify({ query }),
+    })
+    const result = await response.json()
+    return result.data.listWalletIds
+  } catch (err) {
+    return err
   }
-}`
 }
 
 export const GaloyAccountService = (): IAccountService => {
   const fetchAccounts = async (): Promise<Account[] | Error> => {
     try {
       const accounts: Account[] = []
-      const AccountIds = await getAccountIds()
-      for (const accountId of AccountIds) {
-        const query = queryBuilderToFetchWallet(accountId)
+      const accountIds = await getAccountIds()
+      for (const id of accountIds) {
+        const query = gql`query wallet{
+          wallet(walletId: "${id}"){
+            id
+            balance
+          }
+        }`
         const response = await fetch(GALOY_GRAPHQL_ENDPOINT, {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-          },
+          headers,
           body: JSON.stringify({ query }),
         })
-        const data = await response.json()
-        const { wallet } = data.data
+        const result = await response.json()
+        const { wallet } = result.data
         accounts.push({
           accountId: wallet.id,
           balance: wallet.balance,
