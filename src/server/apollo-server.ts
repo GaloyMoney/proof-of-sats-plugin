32c2f7ae9ab5e1fdc075e6437788c5a7026b0a98
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-08-09 12:09:29 +0530

    fix : minor refactors

--- src/server/apollo-server.ts
+++ src/server/apollo-server.ts
@@ -1,23 +1,15 @@
 import { ApolloServer } from "apollo-server"
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
 import { GQLSchema as schema } from "../graphql/index"
-import { pool } from "../services/postgresql"
 
-pool
-  .connect()
-  .then(() => {
-    const server = new ApolloServer({
-      schema: schema,
-      plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
-    })
-    server
-      .listen(4004)
-      .then(({ url }) => {
-        console.log(`🚀  Server ready at ${url}`)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+const server = new ApolloServer({
+  schema: schema,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+})
+server
+  .listen(4004)
+  .then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`)
   })
   .catch((err) => {
     console.log(err)
