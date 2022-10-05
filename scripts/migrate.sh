9a184173a28dad62e454ebb0ee0dcdb785a7fcc2
Vaibhav Dixit <thevaibhavdixit@gmail.com>
2022-10-06 01:40:07 +0530

    feat: add scritps for seeds

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,4 +2,5 @@
 
 node_modules/.bin/knex --knexfile ./database.ts migrate:status
 node_modules/.bin/knex --knexfile ./database.ts migrate:latest
+node_modules/.bin/knex --knexfile ./database.ts seed:run
 node_modules/.bin/knex --knexfile ./database.ts migrate:status
--- src/config/database/seeds/liability-tree.ts
+++ src/config/database/seeds/liability-tree.ts
@@ -1,18 +1,43 @@
 import { Knex } from "knex"
 import { createLiabilitiesTree } from "@galoymoney/proof-of-sats"
 
-import accounts from "./accounts.json"
+const accounts = [
+  {
+    "accountId": "123456789012",
+    "balance": 133145473334
+  },
+  {
+    "accountId": "123456789013",
+    "balance": 235235243567
+  },
+  {
+    "accountId": "123456789014",
+    "balance": 324524334685
+  },
+  {
+    "accountId": "123456789015",
+    "balance": 2342348567432
+  },
+  {
+    "accountId": "123456789016",
+    "balance": 234234213124
+  }
+]
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
-  await knex("table_name").del()
+  await knex("liability_tree").del()
   const { merkleTree, accountToNonceMap } = await createLiabilitiesTree(accounts)
-  const roothash = merkleTree[0][0]
-  const total_balance = merkleTree[0][1]
+  const roothash = merkleTree[0][0].hash
+  const total_balance = merkleTree[0][0].sum
+  const jsonMerkleTree = JSON.stringify(merkleTree)
+  const jsonAccountToNonceMap = JSON.stringify(
+    Array.from(accountToNonceMap.entries()),
+  )
   // Inserts seed entries
   await knex("liability_tree").insert({
-    merkle_tree: merkleTree,
-    account_to_nonce_map: accountToNonceMap,
+    merkle_tree: jsonMerkleTree,
+    account_to_nonce_map: jsonAccountToNonceMap,
     roothash,
   })
   await knex("tree_metadata").insert({
