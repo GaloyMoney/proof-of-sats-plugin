744b544527de96b3bc48b017fe004342b63f271a
Vaibhav Dixit <thevaibhavdixit@gmail.com>
2022-10-25 16:45:00 +0530

    refactor: remove integration-server.ts

--- test/e2e/queries.spec.ts
+++ test/e2e/queries.spec.ts
@@ -1,34 +1,25 @@
 import { gql } from "apollo-server-core"
-import { startServer, killServer, PID } from "../helpers/integration-server"
+
 import axios from "axios"
 
 jest.setTimeout(30000)
 
-let serverPid: PID
-beforeAll(async () => {
-    const serverPid = await startServer("start")
-    console.log(serverPid)
-})
-
-afterAll(async () => {
-    await killServer(serverPid)
-})
-
 describe("graphql", () => {
-    describe("treemetadata query", () => {
-        it("should return a tree metadata", async () => {
-            const query = gql`
-            query treeMetadata{
-                treeMetadata{
-                    roothash
-                    totalBalance
-                    dateCreated
-                }
-            }`
-            const { data } = await axios.post('http://localhost:4004/', { query })
-            expect(data.roothash).toBeTruthy()
-            expect(data.totalBalance).toBeTruthy()
-            expect(data.dateCreated).toBeTruthy()
-        })
+  describe("treemetadata query", () => {
+    it("should return a tree metadata", async () => {
+      const query = gql`
+        query treeMetadata {
+          treeMetadata {
+            roothash
+            totalBalance
+            dateCreated
+          }
+        }
+      `
+      const { data } = await axios.post("http://localhost:4004/", { query })
+      expect(data.roothash).toBeTruthy()
+      expect(data.totalBalance).toBeTruthy()
+      expect(data.dateCreated).toBeTruthy()
     })
+  })
 })
