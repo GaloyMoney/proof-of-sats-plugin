3e164821b242df0371783ae85ea9ddbe8904dc95
Vaibhav Dixit <thevaibhavdixit@gmail.com>
2022-10-31 01:19:15 +0530

    refactor: use fetch instead of axios for gql queries

--- test/e2e/queries.spec.ts
+++ test/e2e/queries.spec.ts
@@ -1,40 +1,45 @@
-import axios from "axios"
-
+import fetch from "node-fetch"
 import { liabilityProofQuery, metadataQuery } from "../queries"
+
+const ServerUrl = "http://localhost:4004/"
+let roothash: string
+const headers = {
+  "Content-Type": "application/json",
+  "Accept": "application/json",
+}
 
 describe("graphql", () => {
   describe("treemetadata query", () => {
     it("should return a tree metadata", async () => {
+      const metadataResponse = await fetch(ServerUrl, {
+        method: "POST",
+        headers,
+        body: JSON.stringify({ query: metadataQuery }),
+      })
       const {
-        data: {
-          data: { treeMetadata },
-        },
-      } = await axios.post("http://localhost:4004", { query: metadataQuery })
-
+        data: { treeMetadata },
+      } = await metadataResponse.json()
       expect(treeMetadata.roothash).toBeTruthy()
       expect(treeMetadata.totalBalance).toBeTruthy()
       expect(treeMetadata.dateCreated).toBeTruthy()
+      if (treeMetadata.roothash) roothash = treeMetadata.roothash
     })
   })
+
   describe("liabilityProof query", () => {
     it("should return a valid liability proof", async () => {
       const accountId = "123456789012"
-      const {
-        data: {
-          data: {
-            treeMetadata: { roothash },
-          },
-        },
-      } = await axios.post("http://localhost:4004/", { query: metadataQuery })
-      const {
-        data: {
-          data: { liabilityProof },
-        },
-      } = await axios.post("http://localhost:4004/", {
-        query: liabilityProofQuery,
-        variables: { accountId, roothash },
+      const liabilityProofResponse = await fetch(ServerUrl, {
+        method: "POST",
+        headers,
+        body: JSON.stringify({
+          query: liabilityProofQuery,
+          variables: { accountId, roothash },
+        }),
       })
-
+      const {
+        data: { liabilityProof },
+      } = await liabilityProofResponse.json()
       expect(liabilityProof.accountId).toBeTruthy()
       expect(liabilityProof.totalBalance).toBeTruthy()
       expect(liabilityProof.nonce).toBeTruthy()
